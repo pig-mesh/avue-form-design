f4b11d741d70a4cc39f658d14b079360cf30df93
lbw <wangiegie@163.com>
2022-09-02 16:08:05 +0800

    :bookmark: Releasing / Version tags.

--- packages/config/table/index.vue
+++ packages/config/table/index.vue
@@ -1,22 +1,44 @@
 <template>
   <div>
     <el-form-item label="字段配置">
-      <el-button type="primary" size="small" @click="$refs.column.visible = true">字段配置</el-button>
+      <el-button
+        type="primary"
+        size="small"
+        @click="$refs.column.visible = true"
+        >字段配置</el-button
+      >
     </el-form-item>
     <el-form-item label="边框">
       <el-switch v-model="data.children.border"></el-switch>
     </el-form-item>
     <el-form-item label="label">
-      <el-input v-model="data.props.label" placeholder="label" clearable></el-input>
+      <el-input
+        v-model="data.props.label"
+        placeholder="label"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="value">
-      <el-input v-model="data.props.value" placeholder="value" clearable></el-input>
+      <el-input
+        v-model="data.props.value"
+        placeholder="value"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="请求地址">
-      <el-input v-model="data.children.props.url" type="textarea" placeholder="请求地址" autosize></el-input>
+      <el-input
+        v-model="data.children.props.url"
+        type="textarea"
+        placeholder="请求地址"
+        autosize
+      ></el-input>
     </el-form-item>
     <el-form-item label="请求方法">
-      <el-select v-model="data.children.props.method" placeholder="请求方法" clearable>
+      <el-select
+        v-model="data.children.props.method"
+        placeholder="请求方法"
+        clearable
+      >
         <el-option label="GET" value="get"></el-option>
         <el-option label="POST" value="post"></el-option>
       </el-select>
@@ -25,37 +47,57 @@
       <el-switch v-model="data.children.props.needPage"></el-switch>
     </el-form-item>
     <el-form-item label="当前页" v-if="data.children.props.needPage">
-      <el-input v-model="data.children.props.currentPageKey" placeholder="当前页" clearable></el-input>
+      <el-input
+        v-model="data.children.props.currentPageKey"
+        placeholder="当前页"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="每页条数" v-if="data.children.props.needPage">
-      <el-input v-model="data.children.props.pageSizeKey" placeholder="每页条数" clearable></el-input>
+      <el-input
+        v-model="data.children.props.pageSizeKey"
+        placeholder="每页条数"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="总条数" v-if="data.children.props.needPage">
-      <el-input v-model="data.children.props.totalKey" placeholder="总条数" clearable></el-input>
+      <el-input
+        v-model="data.children.props.totalKey"
+        placeholder="总条数"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="列表" v-if="data.children.props.needPage">
-      <el-input v-model="data.children.props.recordsKey" placeholder="列表" clearable></el-input>
+      <el-input
+        v-model="data.children.props.recordsKey"
+        placeholder="列表"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="返回层级">
-      <el-input v-model="data.children.props.resKey" placeholder="返回层级" clearable></el-input>
+      <el-input
+        v-model="data.children.props.resKey"
+        placeholder="返回层级"
+        clearable
+      ></el-input>
     </el-form-item>
     <el-form-item label="自动生成">
       <el-switch v-model="data.children.props.auto"></el-switch>
     </el-form-item>
     <div class="el-form--label-top">
       <div class="el-form-item">
-        <label class="el-form-item__label" style="padding: 0;">
+        <label class="el-form-item__label" style="padding: 0">
           <el-popover
-              placement="top-start"
-              :width="200"
-              trigger="hover"
-              content="填写以上配置后会自动生成，若觉得生成的不对请自行修改或关闭自动生成"
+            placement="top-start"
+            :width="200"
+            trigger="hover"
+            content="填写以上配置后会自动生成，若觉得生成的不对请自行修改或关闭自动生成"
           >
             <template #reference>
               <el-link
-                  href="https://avuejs.com/form/form-input-table.html"
-                  :underline="false"
-                  target="_blank"
+                href="https://avuejs.com/form/form-input-table.html"
+                :underline="false"
+                target="_blank"
               >
                 onLoad：
                 <i class="el-icon-question"></i>
@@ -65,66 +107,72 @@
         </label>
         <div class="el-form-item__content">
           <monaco-editor
-              v-model="onLoad"
-              height="200"
-              :keyIndex="`table-onload-${data.prop}`"
-              :options="options"
+            v-model="onLoad"
+            height="200"
+            :keyIndex="`table-onload-${data.prop}`"
+            :options="options"
           ></monaco-editor>
         </div>
       </div>
       <div class="el-form-item">
-        <label class="el-form-item__label" style="padding: 0;">formatter：</label>
+        <label class="el-form-item__label" style="padding: 0"
+          >formatter：</label
+        >
         <div class="el-form-item__content">
           <monaco-editor
-              v-model="formatter"
-              height="200"
-              :keyIndex="`table-formatter-${data.prop}`"
-              :options="options"
+            v-model="formatter"
+            height="200"
+            :keyIndex="`table-formatter-${data.prop}`"
+            :options="options"
           ></monaco-editor>
         </div>
       </div>
     </div>
 
-    <afd-column ref="column" :column="data.children.column" @submit="handleColumn"></afd-column>
+    <afd-column
+      ref="column"
+      :column="data.children.column"
+      @submit="handleColumn"
+    ></afd-column>
   </div>
 </template>
 <script>
-import MonacoEditor from '../../utils/monaco-editor'
-import AfdColumn from './column.vue'
+import MonacoEditor from "../../utils/monaco-editor";
+import AfdColumn from "./column.vue";
 
 export default {
-  name: 'config-table',
-  components: {MonacoEditor, AfdColumn},
-  props: ['data'],
+  name: "config-table",
+  components: { MonacoEditor, AfdColumn },
+  props: ["data"],
   watch: {
-    'data.prop': {
+    "data.prop": {
       handler() {
-        const {onLoad} = this.data
-        this.onLoad = onLoad ? onLoad + '' : ''
+        const { onLoad } = this.data;
+        this.onLoad = onLoad ? onLoad + "" : "";
       },
-      immediate: true
+      immediate: true,
     },
     formatter: {
       handler(val) {
-        this.handleFormatter(val)
+        this.handleFormatter(val);
       },
-      immediate: true
+      immediate: true,
     },
-    'data.children.props': {
+    "data.children.props": {
       handler(val) {
-        this.handleOnLoad(val)
+        this.handleOnLoad(val);
       },
-      deep: true
+      deep: true,
     },
     onLoad: {
       handler(val) {
         try {
-          this.data.onLoad = eval(val)
+          this.data.onLoad = eval(val);
         } catch (e) {
-          console.error(e)
+          console.error(e);
         }
-      }
-    }
+      },
+    },
   },
   data() {
     return {
@@ -134,54 +182,70 @@ export default {
           enabled: false,
         },
       },
-      onLoad: '',
-      formatter: ''
-    }
+      onLoad: "",
+      formatter: "",
+    };
   },
   methods: {
     handleFormatter(obj) {
-      this.formatter = obj ? obj + '' : `(row) => {
+      this.formatter = obj
+        ? obj + ""
+        : `(row) => {
         return row.name
       }`;
-      this.data.formatter = eval(this.formatter)
+      this.data.formatter = eval(this.formatter);
     },
     handleColumn(column) {
-      this.data.children.column = column
+      this.data.children.column = column;
     },
     handleOnLoad(obj) {
-      const {url, method, needPage, currentPageKey, pageSizeKey, totalKey, recordsKey, resKey, auto} = obj
-      if (!auto) return
+      const {
+        url,
+        method,
+        needPage,
+        currentPageKey,
+        pageSizeKey,
+        totalKey,
+        recordsKey,
+        resKey,
+        auto,
+      } = obj;
+      if (!auto) return;
       if (!url) {
-        this.onLoad = `(res, cb) => { }`
-        return
+        this.onLoad = `(res, cb) => { }`;
+        return;
       }
-      let {props} = this.data
-      if (!props) props = {label: 'label', value: 'value'}
-      const {value} = props
+      let { props } = this.data;
+      if (!props) props = { label: "label", value: "value" };
+      const { value } = props;
       const onLoad = `(res, cb) => {
         let { page, value, data } = res
         if (!page) page = { currentPage: 1, pageSize: 10 }
         const { currentPage, pageSize } = page
-        let params = { ...data, ${currentPageKey || 'current'}: currentPage, ${pageSizeKey || 'page'}: pageSize }
+        let params = { ...data, ${currentPageKey || "current"}: currentPage, ${
+        pageSizeKey || "page"
+      }: pageSize }
 
         if (value) params['${value}'] = value
 
-        if ('${method || 'get'}' == 'get') params = { params: { ...params } }
+        if ('${method || "get"}' == 'get') params = { params: { ...params } }
 
         this.$axios['${method}']('${url}', params).then(res => {
-          const response = this.getAsVal(res, '${resKey || 'data.data'}')
+          const response = this.getAsVal(res, '${resKey || "data.data"}')
           if (!response) this.$message.error('未查询到数据或者返回层级配置错误')
 
           let result = {}
           if (${needPage}) {
-            const total = this.getAsVal(response, '${totalKey || 'total'}')
+            const total = this.getAsVal(response, '${totalKey || "total"}')
             if (total || total == 0) {
               result.total = total
             } else {
               this.$message.error('总条数配置错误')
               return
             }
-            const records = this.getAsVal(response, '${recordsKey || 'records'}')
+            const records = this.getAsVal(response, '${
+              recordsKey || "records"
+            }')
             if (records) {
               result.data = records
             } else {
@@ -196,9 +260,9 @@ export default {
             else this.$message.error('根据${value}: ' + value + ' 查询不到结果')
           } else cb(result)
         })
-      }`
-      this.onLoad = onLoad
+      }`;
+      this.onLoad = onLoad;
     },
-  }
-}
+  },
+};
 </script>
